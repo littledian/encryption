4f999628ea3e71822c220049824e4a19dece7475
舒荣贵 <shu@shurongguideMacBook-Pro.local>
2022-03-26 19:01:26 +0800

    test(utils): update sortedArrayInsert test condition

--- utils/sort.test.ts
+++ utils/sort.test.ts
@@ -2,10 +2,12 @@ import { sortedArrayInsert } from '@/utils/sort';
 
 test('test utils/sort:sortedArrayInsert', () => {
   const array = [1, 5, 3, 12, 300, 201, 21, 12, 35];
-  let sortedArray: number[] = [];
-  for (const item of array) {
-    sortedArray = sortedArrayInsert(sortedArray, item, (a, b) => a - b);
-  }
 
+  let sortedArray: number[] = [];
+  for (const item of array) sortedArray = sortedArrayInsert(sortedArray, item, (a, b) => a - b);
   expect(sortedArray).toEqual([1, 3, 5, 12, 12, 21, 35, 201, 300]);
+
+  sortedArray = [];
+  for (const item of array) sortedArray = sortedArrayInsert(sortedArray, item, (a, b) => b - a);
+  expect(sortedArray).toEqual([300, 201, 35, 21, 12, 12, 5, 3, 1]);
 });
